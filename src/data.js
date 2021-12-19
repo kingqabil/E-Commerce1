c2ab6085abc6332006403017f7f62a4b8469d04e
kingqabil <kingqabil@live.com>
2021-12-20 01:42:35 +0300

    Create ProductList Page

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://i.ibb.co/DG69bQ4/2.png",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "fsfafd",
     },
     {
         id: 2,
-        img: "../img/India.jpg alt=Mozilla",
+        img: "https://i.ibb.co/cXFnLLV/3.png",
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "fsfafd",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11067734/2019/12/10/34b589c2-80b9-4ad9-81ea-84333fbc46761575972548055-DILLINGER-Men-Tshirts-4071575972546110-1.jpg",
         title: "POPULAR SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "fsfafd",
@@ -25,12 +25,12 @@ export const sliderItems = [
 export const categories = [
     {
         id: 1,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
         title: "SHIRT STYLE",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://images.pexels.com/photos/7480114/pexels-photo-7480114.jpeg",
         title: "SHIRT LOVE",
     },
     {
@@ -43,19 +43,19 @@ export const categories = [
 export const popularProducts = [
     {
         id: 1,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://cdn11.bigcommerce.com/s-pkla4xn3/images/stencil/608x608/products/24670/224957/New-Autumn-Fashion-Brand-Men-Clothes-Slim-Fit-Men-Long-Sleeve-Shirt-Men-Plaid-Cotton-Casual__62864.1549258439.jpg?c=2",
     },
     {
         id: 2,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://m.media-amazon.com/images/I/61HHwE89xOL._AC_UX385_.jpg",
     },
     {
         id: 3,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ysl-bag-1549362365.jpg",
     },
     {
         id: 4,
-        img: "https://i.ibb.co/T0LW5pp/Indiwa.png",
+        img: "https://cdn.shopclues.com/images1/thumbnails/104223/320/1/148695190-104223920-1598444855.jpg",
     },
     {
         id: 5,
